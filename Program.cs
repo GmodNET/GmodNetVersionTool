5c05cc4923a0ad0e294969106853759af966a825
GlebChili <gleb.chili@gmail.com>
2020-08-15 19:31:48 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using CommandLine;
 
 namespace GmodNET.VersionTool
 {
     class Program
     {
+        class SemVerOptions
+        {
+
+        }
+
+        class AseemblyVerOptions
+        {
+
+        }
+
+        class FileVerOptions
+        {
+            
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
