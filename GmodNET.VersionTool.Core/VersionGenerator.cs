9911c1af8c9b38fb751fdedb7f5c3a855e6b13b1
GlebChili <gleb.chili@gmail.com>
2020-08-20 22:58:55 +0300

    Basic layout

--- GmodNET.VersionTool.Core/VersionGenerator.cs
+++ GmodNET.VersionTool.Core/VersionGenerator.cs
@@ -1,8 +1,33 @@
 ﻿using System;
+using System.IO;
 
 namespace GmodNET.VersionTool.Core
 {
     public class VersionGenerator
     {
+        readonly string full_version;
+        readonly string branch_name;
+        readonly string commit_hash;
+
+        public string FullVersion
+        {
+            get {return full_version;}
+        }
+
+        public string BranchName
+        {
+            get {return branch_name;}
+        }
+
+        public string CommitHash
+        {
+            get {return commit_hash;}
+        }
+
+        public VersionGenerator(string path_to_version_file)
+        {
+            string full_version_file_path = Path.GetFullPath(path_to_version_file);
+            FileInfo version_file_info = new FileInfo(full_version_file_path);
+        }
     }
 }
---
+++ GmodNET.VersionTool.Core/VersionStruct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GmodNET.VersionTool.Core
+{
+    public struct VersionStruct
+    {
+        public string Version {get; set; }
+        public string Codename {get; set; }
+    }
+}
