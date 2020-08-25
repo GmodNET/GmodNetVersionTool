ce82cc17628bab17678429f13542ea56e513dfca
GlebChili <gleb.chili@gmail.com>
2020-08-26 00:04:55 +0300

    Documentation added to VersionStruct

--- GmodNET.VersionTool.Core/VersionStruct.cs
+++ GmodNET.VersionTool.Core/VersionStruct.cs
@@ -4,9 +4,22 @@ using System.Text;
 
 namespace GmodNET.VersionTool.Core
 {
+    /// <summary>
+    /// Represents a structure of JSON version file
+    /// </summary>
     public struct VersionStruct
     {
+        /// <summary>
+        /// Semantic Versioning 2.0.0 compatible version string
+        /// </summary>
         public string Version {get; set; }
+        /// <summary>
+        /// Codename for a version.
+        /// <remarks>
+        /// Can be dropped.
+        /// If specified, must be a single alphanumerical word ([0-9A-Za-z]).
+        /// </remarks>
+        /// </summary>
         public string Codename {get; set; }
     }
 }
