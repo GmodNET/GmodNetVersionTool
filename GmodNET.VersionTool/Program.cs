d7129a80fc2db44da57d80f2525ca5aebf5a7375
GlebChili <gleb.chili@gmail.com>
2020-08-26 19:10:23 +0300

    Console app added

---
+++ GmodNET.VersionTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GmodNET.VersionTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
