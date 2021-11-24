9e72001688c7f404340e2681c28659b388d9a0d6
Gleb Krasilich <gleb.chili@gmail.com>
2021-11-24 12:03:22 +0300

    Added source generator projects

---
+++ GmodNET.VersionTool.SourceGenerator/GmodNET.VersionTool.SourceGenerator/GmodNETVersionToolSourceGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using GmodNET.VersionTool;
+
+namespace GmodNET.VersionTool.SourceGenerator
+{
+    
+}
