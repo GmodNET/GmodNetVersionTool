63e3998ef86626dc903a820beb9c9922ef49b8aa
Gleb Krasilich <gleb.chili@gmail.com>
2021-11-24 14:08:32 +0300

    Remove packaging project

--- GmodNET.VersionTool.SourceGenerator/GmodNET.VersionTool.SourceGenerator/GmodNETVersionToolSourceGenerator.cs
+++ GmodNET.VersionTool.SourceGenerator/GmodNET.VersionTool.SourceGenerator/GmodNETVersionToolSourceGenerator.cs
@@ -7,9 +7,21 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
-using GmodNET.VersionTool;
+using GmodNET.VersionTool.Core;
 
 namespace GmodNET.VersionTool.SourceGenerator
 {
-    
+    [Generator]
+    public class GmodNETVersionToolSourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+    }
 }
